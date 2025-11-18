959e92a03d5a3a948cacab2f552c125f25db73fb
ciroteixeira <ciroby2@hotmail.com>
2025-11-18 17:57:56 +0000

    Changed endpoint hello
    Changed endpoint hello

--- src/main/java/com/cmv/example/controller/HelloWorldController.java
+++ src/main/java/com/cmv/example/controller/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.cmv.example.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping
+	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello, World!";
 	}
